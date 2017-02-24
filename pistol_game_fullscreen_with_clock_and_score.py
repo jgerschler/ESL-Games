6608ca306bc7ebb9c6d8a862ec1b6440858f49c0
jgerschler <jjgerschler@gmail.com>
2017-02-24 13:29:53 -0600

    updated

--- pistol_game_fullscreen_with_clock_and_score.py
+++ pistol_game_fullscreen_with_clock_and_score.py
@@ -111,7 +111,7 @@ class PistolGame(object):
             rect1 = self.message_display_bottomleft(self.word_list[1], (100, self.display_height - 100))
             rect2 = self.message_display_topright(self.word_list[2], (self.display_width - 100, 100))
             rect3 = self.message_display_bottomright(self.word_list[3], (self.display_width - 100, self.display_height - 100))
-            react_score = self.message_display_center(str(self.score), (self.display_width/2, self.display_height - 50))
+            react_score = self.message_display_center("{0} {1}".format(str(self.score),seconds), (self.display_width/2, self.display_height - 50))
             pygame.draw.circle(self.game_display, PistolGame.BLUE, (self.display_width/2, self.display_height/2), 40)# change tracking circle radius as necessary
             if rect0.collidepoint(int_x, int_y) or rect1.collidepoint(int_x, int_y) or rect2.collidepoint(int_x, int_y) or rect3.collidepoint(int_x, int_y):
                 pygame.draw.circle(self.game_display, PistolGame.RED,(int_x, int_y), 10)
