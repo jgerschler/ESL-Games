e6ac97f8b8812ca5dc46b966255ef0a87a2b0b0a
jgerschler <jjgerschler@gmail.com>
2017-03-01 17:08:17 -0600

    added personality adj game

--- personality_adjectives.py
+++ personality_adjectives.py
@@ -2,13 +2,13 @@
 from collections import deque
 from pygame.locals import *
 import numpy as np
-import argparse, imutils, cv2, pygame, time, random
+import argparse, imutils, cv2, pygame, random, sys
 
 class PistolGame(object):
-    DISPLAY_WIDTH = 800# update for full screen later!
-    DISPLAY_HEIGHT = 600
-
-    FONT_SIZE = 64
+    FONT_SIZE = 64# font size for words
+    SCORE_SIZE = 32# font size for score
+    
+    GAME_TIME = 60# number of seconds to complete the game
 
     BLACK = (0,0,0)
     WHITE = (255,255,255)
@@ -31,22 +31,29 @@ class PistolGame(object):
 
         self.finished = False
 
-        self.game_display = pygame.display.set_mode((PistolGame.DISPLAY_WIDTH, PistolGame.DISPLAY_HEIGHT))
+        self.game_display = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
         pygame.display.set_caption('Pistolero Game')
+        self.display_width, self.display_height = pygame.display.get_surface().get_size()
         self.game_display.fill(PistolGame.WHITE)
         pygame.display.update()
 
     def text_objects(self, text, font):
         text_surface = font.render(text, True, PistolGame.BLACK)
         return text_surface, text_surface.get_rect()
+        
+    def message_display_center(self, text, tuple_center):
+        text_surf, text_rect = self.text_objects(text, pygame.font.Font('arial.ttf', PistolGame.SCORE_SIZE))
+        text_rect.center = tuple_center
+        self.game_display.blit(text_surf, text_rect)
+        return text_rect        
 
-    def message_display_topleft(self, text, tuple_topleft):# subdivide these into topleft, topright...etc
+    def message_display_topleft(self, text, tuple_topleft):
         text_surf, text_rect = self.text_objects(text, pygame.font.Font('arial.ttf', PistolGame.FONT_SIZE))
         text_rect.topleft = tuple_topleft
         self.game_display.blit(text_surf, text_rect)
         return text_rect
        
-    def message_display_bottomleft(self, text, tuple_bottomleft):# subdivide these into topleft, topright...etc
+    def message_display_bottomleft(self, text, tuple_bottomleft):
         text_surf, text_rect = self.text_objects(text, pygame.font.Font('arial.ttf', PistolGame.FONT_SIZE))
         text_rect.bottomleft = tuple_bottomleft
         self.game_display.blit(text_surf, text_rect)
@@ -68,20 +75,38 @@ class PistolGame(object):
         self.word_list = random.sample(self.verbs, 3)# update this with dictionary for more flexibility
         self.word_list.append(random.sample(self.adjectives, 1)[0])
         random.shuffle(self.word_list)
+        
+    def end_game(self):
+        self.game_display.fill(PistolGame.WHITE)
+        self.message_display_center("GAME OVER", (self.display_width/2, self.display_height/2))
+        self.message_display_center("SCORE: {0}".format(str(self.score)), (self.display_width/2, self.display_height/3))
+        pygame.display.update()
+        pygame.time.delay(3000)
+        self.finished = True
+        self.camera.release()
+        cv2.destroyAllWindows()
+        pygame.quit()
+        sys.exit()
 
     def run(self):
-        camera = cv2.VideoCapture(0)
+        self.camera = cv2.VideoCapture(0)
         
         self.word_list = random.sample(self.verbs, 3)# update this with dictionary for more flexibility
         self.word_list.append(random.sample(self.adjectives, 1)[0])
         random.shuffle(self.word_list)
 
+        self.score = 0
         int_x, int_y = 0, 0
+        start_ticks = pygame.time.get_ticks()
         
         while not self.finished:
-            (grabbed, frame) = camera.read()
+            seconds = (pygame.time.get_ticks()-start_ticks)/1000
+            if PistolGame.GAME_TIME - seconds == 0:
+                self.end_game()
+            
+            (grabbed, frame) = self.camera.read()
 
-            frame = imutils.resize(frame, width=800)
+            frame = imutils.resize(frame, width=self.display_width)
             hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
             mask = cv2.inRange(hsv, self.object_lower, self.object_upper)
@@ -92,25 +117,22 @@ class PistolGame(object):
             if len(cnts) > 0:
                 c = max(cnts, key=cv2.contourArea)
                 ((x, y), radius) = cv2.minEnclosingCircle(c)
-                int_x, int_y = PistolGame.DISPLAY_WIDTH - int(x), PistolGame.DISPLAY_HEIGHT - int(y)
+                int_x, int_y = self.display_width - int(x), self.display_height - int(y)
                 # if radius > 10:# left here for troubleshooting purposes
                     # cv2.circle(frame, (int_x, int_y), int(radius), (0, 255, 255), 2)
             # cv2.imshow("Frame", frame)
             
-            try:
-                self.game_display.fill(PistolGame.WHITE)
-                rect0 = self.message_display_topleft(self.word_list[0], (100, 100))
-                rect1 = self.message_display_bottomleft(self.word_list[1], (100, PistolGame.DISPLAY_HEIGHT - 100))
-                rect2 = self.message_display_topright(self.word_list[2], (PistolGame.DISPLAY_WIDTH - 100, 100))
-                rect3 = self.message_display_bottomright(self.word_list[3], (PistolGame.DISPLAY_WIDTH - 100, PistolGame.DISPLAY_HEIGHT - 100))
-                pygame.draw.circle(self.game_display, PistolGame.BLUE, (PistolGame.DISPLAY_WIDTH/2, PistolGame.DISPLAY_HEIGHT/2), 60)
-                if rect0.collidepoint(int_x, int_y) or rect1.collidepoint(int_x, int_y) or rect2.collidepoint(int_x, int_y) or rect3.collidepoint(int_x, int_y):
-                    pygame.draw.circle(self.game_display, PistolGame.RED,(int_x, int_y), 10)
-                else:
-                    pygame.draw.circle(self.game_display, PistolGame.BLACK,(int_x, int_y), 10)
-                
-            except:
-                pass# temporary! add error handling!
+            self.game_display.fill(PistolGame.WHITE)
+            rect0 = self.message_display_topleft(self.word_list[0], (100, 100))
+            rect1 = self.message_display_bottomleft(self.word_list[1], (100, self.display_height - 100))
+            rect2 = self.message_display_topright(self.word_list[2], (self.display_width - 100, 100))
+            rect3 = self.message_display_bottomright(self.word_list[3], (self.display_width - 100, self.display_height - 100))
+            react_score = self.message_display_center("{0} {1}".format(str(self.score), PistolGame.GAME_TIME - seconds), (self.display_width/2, self.display_height - 50))
+            pygame.draw.circle(self.game_display, PistolGame.BLUE, (self.display_width/2, self.display_height/2), 40)# change tracking circle radius as necessary
+            if rect0.collidepoint(int_x, int_y) or rect1.collidepoint(int_x, int_y) or rect2.collidepoint(int_x, int_y) or rect3.collidepoint(int_x, int_y):
+                pygame.draw.circle(self.game_display, PistolGame.RED,(int_x, int_y), 10)
+            else:
+                pygame.draw.circle(self.game_display, PistolGame.BLACK,(int_x, int_y), 10)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -119,41 +141,49 @@ class PistolGame(object):
                     # update to use dictionary here?
                     if (rect0.collidepoint(int_x, int_y) and self.word_list[0] in self.adjectives):
                         self.sound_shot.play()
+                        self.score+=1
                         self.game_display.blit(self.image_shot, (rect0.center[0]-self.image_shot.get_width()/2, rect0.center[1]-self.image_shot.get_height()/2))
                         pygame.display.update()
                         pygame.time.delay(300)
                     elif (rect1.collidepoint(int_x, int_y) and self.word_list[1] in self.adjectives):
                         self.sound_shot.play()
+                        self.score+=1
                         self.game_display.blit(self.image_shot, (rect1.center[0]-self.image_shot.get_width()/2, rect1.center[1]-self.image_shot.get_height()/2))
                         pygame.display.update()
                         pygame.time.delay(300)
                     elif (rect2.collidepoint(int_x, int_y) and self.word_list[2] in self.adjectives):
                         self.sound_shot.play()
+                        self.score+=1
                         self.game_display.blit(self.image_shot, (rect2.center[0]-self.image_shot.get_width()/2, rect2.center[1]-self.image_shot.get_height()/2))
                         pygame.display.update()
                         pygame.time.delay(300)
                     elif (rect3.collidepoint(int_x, int_y) and self.word_list[3] in self.adjectives):
                         self.sound_shot.play()
+                        self.score+=1
                         self.game_display.blit(self.image_shot, (rect3.center[0]-self.image_shot.get_width()/2, rect3.center[1]-self.image_shot.get_height()/2))
                         pygame.display.update()
                         pygame.time.delay(300)
                     elif (rect0.collidepoint(int_x, int_y) and self.word_list[0] in self.verbs):
                         self.sound_shot.play()
+                        self.score-=1
                         self.game_display.blit(self.image_shot, (rect0.center[0]-self.image_shot.get_width()/2, rect0.center[1]-self.image_shot.get_height()/2))
                         pygame.display.update()
                         pygame.time.delay(300)
                     elif (rect1.collidepoint(int_x, int_y) and self.word_list[1] in self.verbs):
                         self.sound_shot.play()
+                        self.score-=1
                         self.game_display.blit(self.image_shot, (rect1.center[0]-self.image_shot.get_width()/2, rect1.center[1]-self.image_shot.get_height()/2))
                         pygame.display.update()
                         pygame.time.delay(300)
                     elif (rect2.collidepoint(int_x, int_y) and self.word_list[2] in self.verbs):
                         self.sound_shot.play()
+                        self.score-=1
                         self.game_display.blit(self.image_shot, (rect2.center[0]-self.image_shot.get_width()/2, rect2.center[1]-self.image_shot.get_height()/2))
                         pygame.display.update()
                         pygame.time.delay(300)
                     elif (rect3.collidepoint(int_x, int_y) and self.word_list[3] in self.verbs):
                         self.sound_shot.play()
+                        self.score-=1
                         self.game_display.blit(self.image_shot, (rect3.center[0]-self.image_shot.get_width()/2, rect3.center[1]-self.image_shot.get_height()/2))
                         pygame.display.update()
                         pygame.time.delay(300)
