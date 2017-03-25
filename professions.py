3f52fe6d183b6b92c3fef3e12b7b1a575df488f7
jgerschler <jjgerschler@gmail.com>
2017-03-25 11:27:51 -0600

    a couple of scripts still need to have some code optimized, but everything should be working.

--- professions.py
+++ professions.py
@@ -60,36 +60,16 @@ class PistolGame(object):
         text_surface = font.render(text, True, PistolGame.BLACK)
         return text_surface, text_surface.get_rect()
         
-    # these message display functions need to be combined!    
-    def message_display_center(self, text, tuple_center):
-        text_surf, text_rect = self.text_objects(text, pygame.font.Font('arial.ttf', PistolGame.SCORE_SIZE))
-        text_rect.center = tuple_center
-        self.game_display.blit(text_surf, text_rect)
-        return text_rect        
+    # these message display functions need to be combined!
 
-    def message_display_topleft(self, text, tuple_topleft):
-        text_surf, text_rect = self.text_objects(text, pygame.font.Font('arial.ttf', PistolGame.FONT_SIZE))
-        text_rect.topleft = tuple_topleft
+    def message_display(self, text, loc_tuple, loc_int, score_flag):# loc_int: 1 center, 2 top left, 3 bottom left, 4 bottom right, 5 top right. score_flag; 0 default font size, 1 score font size
+        text_surf, text_rect = self.text_objects(text, pygame.font.Font('arial.ttf', PistolGame.FONT_SIZE))# improve this section
+        if score_flag == 1:
+            text_surf, text_rect = self.text_objects(text, pygame.font.Font('arial.ttf', PistolGame.SCORE_SIZE))
+        loc_dict = {1:'text_rect.center', 2:'text_rect.topleft', 3:'text_rect.bottomleft', 4:'text_rect.bottomright', 5:'text_rect.topright'}
+        exec(loc_dict[loc_int] + ' = loc_tuple')
         self.game_display.blit(text_surf, text_rect)
-        return text_rect
-       
-    def message_display_bottomleft(self, text, tuple_bottomleft):
-        text_surf, text_rect = self.text_objects(text, pygame.font.Font('arial.ttf', PistolGame.FONT_SIZE))
-        text_rect.bottomleft = tuple_bottomleft
-        self.game_display.blit(text_surf, text_rect)
-        return text_rect
-        
-    def message_display_topright(self, text, tuple_topright):
-        text_surf, text_rect = self.text_objects(text, pygame.font.Font('arial.ttf', PistolGame.FONT_SIZE))
-        text_rect.topright = tuple_topright
-        self.game_display.blit(text_surf, text_rect)
-        return text_rect
-        
-    def message_display_bottomright(self, text, tuple_bottomright):
-        text_surf, text_rect = self.text_objects(text, pygame.font.Font('arial.ttf', PistolGame.FONT_SIZE))
-        text_rect.bottomright = tuple_bottomright
-        self.game_display.blit(text_surf, text_rect)
-        return text_rect
+        return text_rect   
         
     def new_round(self):
         self.selected_profession = random.choice(self.translations.keys())
@@ -101,8 +81,8 @@ class PistolGame(object):
         
     def end_game(self):
         self.game_display.fill(PistolGame.WHITE)
-        self.message_display_center("GAME OVER", (self.display_width/2, self.display_height/2))
-        self.message_display_center("SCORE: {0}".format(str(self.score)), (self.display_width/2, self.display_height/3))
+        self.message_display("GAME OVER", (self.display_width/2, self.display_height/2), 1, 0)
+        self.message_display("SCORE: {0}".format(str(self.score)), (self.display_width/2, self.display_height/3), 1, 0)
         pygame.display.update()
         pygame.time.delay(3000)
         self.finished = True
@@ -149,12 +129,12 @@ class PistolGame(object):
             # cv2.imshow("Frame", frame)
             
             self.game_display.fill(PistolGame.WHITE)
-            rect0 = self.message_display_topleft(self.professions_list[0], (100, 100))
-            rect1 = self.message_display_bottomleft(self.professions_list[1], (100, self.display_height - 100))
-            rect2 = self.message_display_topright(self.professions_list[2], (self.display_width - 100, 100))
-            rect3 = self.message_display_bottomright(self.professions_list[3], (self.display_width - 100, self.display_height - 100))
-            react_score = self.message_display_center("{0} {1}".format(str(self.score), PistolGame.GAME_TIME - seconds), (self.display_width/2, self.display_height - 50))
-            base_verb = self.message_display_center(self.translations[self.selected_profession], (self.display_width/2, 50))
+            rect0 = self.message_display(self.professions_list[0], (100, 100), 2, 0)
+            rect1 = self.message_display(self.professions_list[1], (100, self.display_height - 100), 3, 0)
+            rect2 = self.message_display(self.professions_list[2], (self.display_width - 100, 100), 5, 0)
+            rect3 = self.message_display(self.professions_list[3], (self.display_width - 100, self.display_height - 100), 4, 0)
+            react_score = self.message_display("{0} {1}".format(str(self.score), PistolGame.GAME_TIME - seconds), (self.display_width/2, self.display_height - 50), 1, 1)
+            base_verb = self.message_display(self.translations[self.selected_profession], (self.display_width/2, 50), 1, 1)
             pygame.draw.circle(self.game_display, PistolGame.BLUE, (self.display_width/2, self.display_height/2), 40)# change tracking circle radius as necessary
             if rect0.collidepoint(int_x, int_y) or rect1.collidepoint(int_x, int_y) or rect2.collidepoint(int_x, int_y) or rect3.collidepoint(int_x, int_y):
                 pygame.draw.circle(self.game_display, PistolGame.RED,(int_x, int_y), 10)
