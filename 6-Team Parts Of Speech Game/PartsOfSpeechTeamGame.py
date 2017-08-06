ff7a81188e53ab8d9d95aa28f008f60169a93f96
Jared Gerschler <jjgerschler@gmail.com>
2017-08-06 12:39:06 -0500

    not complete!!!

---
+++ 6-Team Parts Of Speech Game/PartsOfSpeechTeamGame.py
@@ -0,0 +1,343 @@
+import pygame, sys
+from pygame.locals import *
+from random import randint
+
+pygame.init()
+
+xRes = 1024
+yRes = 768
+
+vocabtuples = (('agree','verb'),('allow','verb'),('appear','verb'),('ask','verb'),('be','verb'),('become','verb'),
+               ('begin','verb'),('believe','verb'),('belong','verb'),('bring','verb'),('build','verb'),('carry','verb'),
+               ('choose','verb'),('come','verb'),('connect','verb'),('consider','verb'),('continue','verb'),
+               ('contribute','verb'),('correct','verb'),('create','verb'),('decide','verb'),('deliver','verb'),
+               ('destroy','verb'),('develop','verb'),('discover','verb'),('discuss','verb'),('eat','verb'),('encourage','verb'),
+               ('explain','verb'),('follow','verb'),('get','verb'),('give','verb'),('go','verb'),('happen','verb'),
+               ('have','verb'),('hear','verb'),('imagine','verb'),('include','verb'),('involve','verb'),('know','verb'),
+               ('learn','verb'),('let','verb'),('lose','verb'),('make','verb'),('obtain','verb'),('open','verb'),('pay','verb'),
+               ('read','verb'),('realize','verb'),('receive','verb'),('remember','verb'),('say','verb'),('see','verb'),
+               ('seem','verb'),('sell','verb'),('send','verb'),('serve','verb'),('sit','verb'),('speak','verb'),('spend','verb'),
+               ('suffer','verb'),('suggest','verb'),('take','verb'),('teach','verb'),('tell','verb'),('think','verb'),
+               ('try','verb'),('understand','verb'),('want','verb'),('write','verb'),
+
+               ('red','adj'),('green','adj'),('purple','adj'),('yellow','adj'),('brown','adj'),('different','adj'),
+               ('important','adj'),('other','adj'),('new','adj'),('old','adj'),('young','adj'),('fat','adj'),('skinny','adj'),
+               ('pretty','adj'),('ugly','adj'),('beautiful','adj'),('nice','adj'),('fantastic','adj'),('long','adj'),('short','adj'),
+               ('tall','adj'),('big','adj'),('small','adj'),('political','adj'),('best','adj'),('worst','adj'),('happiest','adj'),
+               ('saddest','adj'),('flirtatious','adj'),('stinky','adj'),('smelly','adj'),('squishy','adj'),('greasy','adj'),
+               ('hot','adj'),('cold','adj'),('warm','adj'),('environmental','adj'),('financial','adj'),('scientific','adj'),
+               ('medical','adj'),('smart','adj'),('dumb','adj'),('hairy','adj'),('smooth','adj'),('rough','adj'),('lonely','adj'),
+               ('natural','adj'),('wrong','adj'),('incorrect','adj'),('correct','adj'),('afraid','adj'),('alive','adj'),('bad','adj'),
+               ('good','adj'),('annoying','adj'),('irritating','adj'),('brave','adj'),('broken','adj'),('cheap','adj'),
+               ('expensive','adj'),('dangerous','adj'),('empty','adj'),('full','adj'),('dry','adj'),('wet','adj'),('exciting','adj'),
+               ('boring','adj'),('great','adj'),
+
+               ('year','noun'),('people','noun'),('way','noun'),('day','noun'),('man','noun'),('thing','noun'),('woman','noun'),
+               ('life','noun'),('child','noun'),('world','noun'),('family','noun'),('student','noun'),('country','noun'),
+               ('problem','noun'),('week','noun'),('company','noun'),('system','noun'),('government','noun'),('night','noun'),
+               ('house','noun'),('car','noun'),('I','noun'),('you','noun'),('they','noun'),('he','noun'),('she','noun'),('it','noun'),
+               ('we','noun'),('book','noun'),('elephant','noun'),('cat','noun'),('dog','noun'),('hippopotamus','noun'),
+               ('magazine','noun'),('eye','noun'),('leg','noun'),('brain','noun'),('job','noun'),('business','noun'),
+               ('teacher','noun'),('mother','noun'),('father','noun'),('boy','noun'),('girl','noun'),('triangle','noun'),
+               ('rectangle','noun'),('history','noun'),('war','noun'),('art','noun'),('science','noun'),('nursing','noun'),
+               ('chemistry','noun'),('biotechnology','noun'),('money','noun'),('person','noun'),('health','noun'),('door','noun'),
+               ('window','noun'),('ceiling','noun'),('roof','noun'),('office','noun'),('computer','noun'),('Xbox','noun'),
+               ('stethescope','noun'),('injection','noun'),('tree','noun'),('boat','noun'),('river','noun'),('lake','noun'),
+               ('sky','noun'),('mango','noun'))
+
+#colors
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+
+#customize team names
+
+team1Name = 'Team 1'
+team2Name = 'Team 2'
+team3Name = 'Team 3'
+team4Name = 'Team 4'
+team5Name = 'Team 5'
+team6Name = 'Team 6'
+
+#starting scores (add handicap as necessary). Will affect answers required to win, effect varies based on screen resolution.
+
+team1Score = 0
+team2Score = 0
+team3Score = 0
+team4Score = 0
+team5Score = 0
+team6Score = 0
+
+#how many correct answers to win?
+
+correctAnswersToWin = 20
+winningScore = (yRes-40)/2#yRes minus 40px team name rectangle
+pointsPerQuestion = winningScore/correctAnswersToWin
+
+#starting word
+
+activeWord = ''
+activeWordClass = ''
+
+def RefreshDisplay():
+    team1Label = teamfont.render(team1Name, True, BLACK)
+    team2Label = teamfont.render(team2Name, True, BLACK)
+    team3Label = teamfont.render(team3Name, True, BLACK)
+    team4Label = teamfont.render(team4Name, True, BLACK)
+    team5Label = teamfont.render(team5Name, True, BLACK)
+    team6Label = teamfont.render(team6Name, True, BLACK)
+
+    team1LabelPos = team1Label.get_rect()
+    team2LabelPos = team2Label.get_rect()
+    team3LabelPos = team3Label.get_rect()
+    team4LabelPos = team4Label.get_rect()
+    team5LabelPos = team5Label.get_rect()
+    team6LabelPos = team6Label.get_rect()
+
+    team1LabelPos.centerx = (3*xRes/16)/2
+    team2LabelPos.centerx = ((2*xRes/16)+(xRes/10)+(2*xRes/16)+(xRes/10)+(xRes/16))/2
+    team3LabelPos.centerx = ((3*xRes/16)+(2*xRes/10)+(3*xRes/16)+(2*xRes/10)+(xRes/16))/2
+    team4LabelPos.centerx = ((4*xRes/16)+(3*xRes/10)+(4*xRes/16)+(3*xRes/10)+(xRes/16))/2
+    team5LabelPos.centerx = ((5*xRes/16)+(4*xRes/10)+(5*xRes/16)+(4*xRes/10)+(xRes/16))/2
+    team6LabelPos.centerx = ((6*xRes/16)+(5*xRes/10)+(6*xRes/16)+(5*xRes/10)+(xRes/16))/2
+
+    team1LabelPos.centery = (2*yRes-40)/2
+    team2LabelPos.centery = (2*yRes-40)/2
+    team3LabelPos.centery = (2*yRes-40)/2
+    team4LabelPos.centery = (2*yRes-40)/2
+    team5LabelPos.centery = (2*yRes-40)/2
+    team6LabelPos.centery = (2*yRes-40)/2
+
+    DISPLAYSURF.blit(team1Label,team1LabelPos)
+    DISPLAYSURF.blit(team2Label,team2LabelPos)
+    DISPLAYSURF.blit(team3Label,team3LabelPos)
+    DISPLAYSURF.blit(team4Label,team4LabelPos)
+    DISPLAYSURF.blit(team5Label,team5LabelPos)
+    DISPLAYSURF.blit(team6Label,team6LabelPos)
+
+    team1Rect = pygame.Rect(xRes/16,yRes-team1Score-40,xRes/16,team1Score)
+    team2Rect = pygame.Rect((2*xRes/16)+(xRes/10),yRes-team2Score-40,xRes/16,team2Score)
+    team3Rect = pygame.Rect((3*xRes/16)+(2*xRes/10),yRes-team3Score-40,xRes/16,team3Score)
+    team4Rect = pygame.Rect((4*xRes/16)+(3*xRes/10),yRes-team4Score-40,xRes/16,team4Score)
+    team5Rect = pygame.Rect((5*xRes/16)+(4*xRes/10),yRes-team5Score-40,xRes/16,team5Score)
+    team6Rect = pygame.Rect((6*xRes/16)+(5*xRes/10),yRes-team6Score-40,xRes/16,team6Score)
+
+    pygame.draw.rect(DISPLAYSURF, RED, team1Rect)
+    pygame.draw.rect(DISPLAYSURF, RED, team2Rect)
+    pygame.draw.rect(DISPLAYSURF, RED, team3Rect)
+    pygame.draw.rect(DISPLAYSURF, RED, team4Rect)
+    pygame.draw.rect(DISPLAYSURF, RED, team5Rect)
+    pygame.draw.rect(DISPLAYSURF, RED, team6Rect)
+
+    pygame.draw.line(DISPLAYSURF, BLUE, (0, (yRes-40)/2), (xRes, (yRes-40)/2), 4)
+
+    pygame.display.update()
+    return
+
+def GameOver(team):
+
+    global team1Score
+    global team2Score
+    global team3Score
+    global team4Score
+    global team5Score
+    global team6Score
+    global activeWord
+    global activeWordClass
+    
+    DISPLAYSURF.fill(WHITE)
+
+    text = font.render(team+' wins!', True, RED)
+    textpos = text.get_rect()
+    textpos.centerx = DISPLAYSURF.get_rect().centerx
+    textpos.y = yRes/4
+    DISPLAYSURF.blit(text,textpos)
+
+    RefreshDisplay()
+
+    team1Score = 0
+    team2Score = 0
+    team3Score = 0
+    team4Score = 0
+    team5Score = 0
+    team6Score = 0
+
+    randomWordInt = randint(0,len(vocabtuples)-1)
+    activeWord = vocabtuples[randomWordInt][0]
+    activeWordClass = vocabtuples[randomWordInt][1]
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYUP and event.key == pygame.K_SPACE:
+                return
+            
+def DeactivateKeys():
+    DISPLAYSURF.fill(WHITE)
+
+    text = font.render(activeWord, True, BLACK)
+    textpos = text.get_rect()
+    textpos.centerx = DISPLAYSURF.get_rect().centerx
+    textpos.y = yRes/4
+    DISPLAYSURF.blit(text,textpos)
+
+    RefreshDisplay()
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYUP and event.key == pygame.K_SPACE:
+                return
+
+def NewWord():
+    global activeWord
+    global activeWordClass
+    randomWordInt = randint(0,len(vocabtuples)-1)
+    activeWord = vocabtuples[randomWordInt][0]
+    activeWordClass = vocabtuples[randomWordInt][1]
+    
+    DISPLAYSURF.fill(WHITE)
+
+    text = font.render(activeWord, True, BLACK)
+    textpos = text.get_rect()
+    textpos.centerx = DISPLAYSURF.get_rect().centerx
+    textpos.y = yRes/4
+    DISPLAYSURF.blit(text,textpos)
+
+    RefreshDisplay()
+
+def team1ScoreUpdate(score):
+    global team1Score
+    global team1Name
+    global winningScore
+    team1Score += score
+    if team1Score < 0:
+        team1Score = 0
+    if team1Score >= winningScore:
+        GameOver(team1Name)
+
+def team2ScoreUpdate(score):
+    global team2Score
+    global team2Name
+    global winningScore
+    team2Score += score
+    if team2Score < 0:
+        team2Score = 0
+    if team2Score >= winningScore:
+        GameOver(team2Name)
+
+def team3ScoreUpdate(score):
+    global team3Score
+    global team3Name
+    global winningScore
+    team3Score += score
+    if team3Score < 0:
+        team3Score = 0
+    if team3Score >= winningScore:
+        GameOver(team3Name)
+
+def team4ScoreUpdate(score):
+    global team4Score
+    global team4Name
+    global winningScore
+    team4Score += score
+    if team4Score < 0:
+        team4Score = 0
+    if team4Score >= winningScore:
+        GameOver(team4Name)
+
+def team5ScoreUpdate(score):
+    global team5Score
+    global team5Name
+    global winningScore
+    team5Score += score
+    if team5Score < 0:
+        team5Score = 0
+    if team5Score >= winningScore:
+        GameOver(team5Name)
+
+def team6ScoreUpdate(score):
+    global team6Score
+    global team6Name
+    global winningScore
+    team6Score += score
+    if team6Score < 0:
+        team6Score = 0
+    if team6Score >= winningScore:
+        GameOver(team6Name)
+
+keybindingsdict = {pygame.K_a:team1ScoreUpdate,
+                   pygame.K_e:team2ScoreUpdate,
+                   pygame.K_i:team3ScoreUpdate,
+                   pygame.K_m:team4ScoreUpdate,
+                   pygame.K_q:team5ScoreUpdate,
+                   pygame.K_u:team6ScoreUpdate,
+                   pygame.K_b:team1ScoreUpdate,
+                   pygame.K_f:team2ScoreUpdate,
+                   pygame.K_j:team3ScoreUpdate,
+                   pygame.K_n:team4ScoreUpdate,
+                   pygame.K_r:team5ScoreUpdate,
+                   pygame.K_v:team6ScoreUpdate,
+                   pygame.K_c:team1ScoreUpdate,
+                   pygame.K_g:team2ScoreUpdate,
+                   pygame.K_k:team3ScoreUpdate,
+                   pygame.K_o:team4ScoreUpdate,
+                   pygame.K_s:team5ScoreUpdate,
+                   pygame.K_w:team6ScoreUpdate}
+
+#display prep
+
+DISPLAYSURF = pygame.display.set_mode((xRes, yRes), 0, 32)
+
+pygame.display.set_caption('Testing')
+
+#starting text and display
+
+DISPLAYSURF.fill(WHITE)
+
+font = pygame.font.SysFont(None, 72)
+teamfont = pygame.font.SysFont(None, 32)
+
+RefreshDisplay()
+
+#main loop
+
+while True:
+
+    for event in pygame.event.get():
+
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_SPACE:
+                NewWord()
+            if event.key in (pygame.K_a,pygame.K_e,pygame.K_i,pygame.K_m,pygame.K_q,pygame.K_u):
+                if activeWordClass == 'adj':
+                    keybindingsdict[event.key](pointsPerQuestion)
+                    DeactivateKeys()#alter as needed to affect classroom speed, use time.sleep if needed
+                else:
+                    keybindingsdict[event.key](-pointsPerQuestion)
+                    DeactivateKeys()
+            if event.key in (pygame.K_b,pygame.K_f,pygame.K_j,pygame.K_n,pygame.K_r,pygame.K_v):
+                if activeWordClass == 'noun':
+                    keybindingsdict[event.key](pointsPerQuestion)
+                    DeactivateKeys()
+                else:
+                    keybindingsdict[event.key](-pointsPerQuestion)
+                    DeactivateKeys()
+            if event.key in (pygame.K_c,pygame.K_g,pygame.K_k,pygame.K_o,pygame.K_s,pygame.K_w):
+                if activeWordClass == 'verb':
+                    keybindingsdict[event.key](pointsPerQuestion)
+                    DeactivateKeys()
+                else:
+                    keybindingsdict[event.key](-pointsPerQuestion)
+                    DeactivateKeys()
+
+    pygame.display.update()
---
+++ SentenceBuilder/sentencebuilder.py
@@ -0,0 +1,298 @@
+from pygame.locals import *
+import pygame
+import time
+import sqlite3
+import math
+import random
+import pygame.font
+
+class TextRectException:
+    def __init__(self, message = None):
+        self.message = message
+    def __str__(self):
+        return self.message
+
+class SentenceBuilder(object):
+    WHITE = (255,255,255)
+    BLACK = (0,0,0)
+    GREEN = (0,128,0)
+    YELLOW = (230,230,0)
+    RED = (255,0,0)
+    BLUE = (0,0,255)
+    BROWN = (97,65,38)
+
+    def __init__(self):
+        pygame.init()
+        pygame.mixer.init()
+
+        self.sound_win = pygame.mixer.Sound('audio\\ping.ogg')
+        self.sound_loss = pygame.mixer.Sound('audio\\buzzer.ogg')         
+
+
+
+finished = False
+
+constructedsentence = ""
+
+
+
+def render_textrect(string, font, rect, text_color, background_color, justification=0):
+    
+    final_lines = []
+
+    requested_lines = string.splitlines()
+
+    for requested_line in requested_lines:
+        if font.size(requested_line)[0] > rect.width:
+            words = requested_line.split(' ')
+            for word in words:
+                if font.size(word)[0] >= rect.width:
+                    raise TextRectException("The word " + word + " is too long to fit in the rect passed.")
+            accumulated_line = ""
+            for word in words:
+                test_line = accumulated_line + word + " "   
+                if font.size(test_line)[0] < rect.width:
+                    accumulated_line = test_line 
+                else: 
+                    final_lines.append(accumulated_line) 
+                    accumulated_line = word + " " 
+            final_lines.append(accumulated_line)
+        else: 
+            final_lines.append(requested_line) 
+
+    surface = pygame.Surface(rect.size) 
+    surface.fill(background_color) 
+
+    accumulated_height = 0 
+    for line in final_lines: 
+        if accumulated_height + font.size(line)[1] >= rect.height:
+            raise TextRectException("Once word-wrapped, the text string was too tall to fit in the rect.")
+        if line != "":
+            tempsurface = font.render(line, 1, text_color)
+            if justification == 0:
+                surface.blit(tempsurface, (0, accumulated_height))
+            elif justification == 1:
+                surface.blit(tempsurface, ((rect.width - tempsurface.get_width()) / 2, accumulated_height))
+            elif justification == 2:
+                surface.blit(tempsurface, (rect.width - tempsurface.get_width(), accumulated_height))
+            else:
+                raise TextRectException("Invalid justification argument: " + str(justification))
+        accumulated_height += font.size(line)[1]
+
+    return surface
+
+def SentenceGen(sentence):
+    sentencelist = sentence.split(' ')
+    fragmentlist = []
+    i = 0
+    j = 0
+    numberofbuttons = 4#number of buttons on controller
+
+    fragsizefloor = int(math.floor(len(sentencelist)/(numberofbuttons-1)))
+    fragsizeremainder = len(sentencelist)%(numberofbuttons-1)
+
+    for i in range(numberofbuttons-1):
+        fragment = ""
+        for j in range(fragsizefloor):
+            fragment = fragment + " " + sentencelist[fragsizefloor*i+j]
+        fragmentlist.append(fragment)
+    #clean up leading space
+    i = 0
+    for i in range(len(fragmentlist)):
+        fragmentlist[i] = fragmentlist[i][1:]
+
+    if fragsizeremainder != 0:
+        fragmentlist.append(" ".join(sentencelist[len(sentencelist)-fragsizeremainder:]))
+
+    random.shuffle(fragmentlist)
+    #make sure there are always the correct number of entries.
+    if fragsizeremainder == 0:
+        fragmentlist.append("")
+
+    return fragmentlist
+
+def NewUser():
+    global frag0
+    global frag1
+    global frag2
+    global frag3
+    global username
+    global sentence
+    global constructedsentence
+
+    constructedsentence = ""
+    c.execute('select * from users order by random() limit 1;')
+    userdata = c.fetchone()
+    username = str(userdata[1])
+    c.execute('select * from sentences order by random() limit 1;')
+    sentencedata = c.fetchone()
+    sentence = str(sentencedata[1])
+    fragmentlist = SentenceGen(sentence)
+    frag0 = fragmentlist[0]
+    frag1 = fragmentlist[1]
+    frag2 = fragmentlist[2]
+    frag3 = fragmentlist[3]
+
+    display.fill(WHITE)
+    #rendered_text = render_textrect(sentenceunderline, my_font, my_rect, BLACK, WHITE, 1)
+    rendered_text_user = render_textrect(username, my_font, my_rect_user, BROWN, WHITE, 0)#last 0 is to left align
+    rendered_text_frag_1 = render_textrect(frag0, my_font, my_rect_frag_1, RED, WHITE, 0)
+    rendered_text_frag_2 = render_textrect(frag1, my_font, my_rect_frag_2, YELLOW, WHITE, 0)
+    rendered_text_frag_3 = render_textrect(frag2, my_font, my_rect_frag_3, GREEN, WHITE, 0)
+    rendered_text_frag_4 = render_textrect(frag3, my_font, my_rect_frag_4, BLUE, WHITE, 0)
+
+    #display.blit(rendered_text, my_rect.topleft)
+    display.blit(rendered_text_user, my_rect_user.topleft)
+    display.blit(rendered_text_frag_1, my_rect_frag_1.topleft)
+    display.blit(rendered_text_frag_2, my_rect_frag_2.topleft)
+    display.blit(rendered_text_frag_3, my_rect_frag_3.topleft)
+    display.blit(rendered_text_frag_4, my_rect_frag_4.topleft)
+
+    pygame.display.update()
+
+    return
+
+def RefreshScreen(fragment):
+    global sentence
+    global constructedsentence
+    global frag0
+    global frag1
+    global frag2
+    global frag3
+    global username
+
+    if constructedsentence == "":
+        constructedsentence = constructedsentence + fragment
+    else:
+        constructedsentence = constructedsentence + " " + fragment
+
+    if fragment == frag0:
+        frag0 = ""
+    elif fragment == frag1:
+        frag1 = ""
+    elif fragment == frag2:
+        frag2 = ""
+    elif fragment == frag3:
+        frag3 = ""
+
+    if frag0 == frag1 == frag2 == frag3 == "" and sentence == constructedsentence:#winner!
+        display.fill(WHITE)
+        rendered_text = render_textrect(sentence, my_font, my_rect, GREEN, WHITE, 1)
+        rendered_text_user = render_textrect(username, my_font, my_rect_user, BROWN, WHITE, 0)#last 0 is to left align
+        rendered_text_frag_1 = render_textrect(frag0, my_font, my_rect_frag_1, RED, WHITE, 0)
+        rendered_text_frag_2 = render_textrect(frag1, my_font, my_rect_frag_2, YELLOW, WHITE, 0)
+        rendered_text_frag_3 = render_textrect(frag2, my_font, my_rect_frag_3, GREEN, WHITE, 0)
+        rendered_text_frag_4 = render_textrect(frag3, my_font, my_rect_frag_4, BLUE, WHITE, 0)
+
+        display.blit(rendered_text, my_rect.topleft)
+        display.blit(rendered_text_user, my_rect_user.topleft)
+        display.blit(rendered_text_frag_1, my_rect_frag_1.topleft)
+        display.blit(rendered_text_frag_2, my_rect_frag_2.topleft)
+        display.blit(rendered_text_frag_3, my_rect_frag_3.topleft)
+        display.blit(rendered_text_frag_4, my_rect_frag_4.topleft)
+
+        pygame.display.update()
+        soundwin.play()
+
+        return
+
+    elif frag0 == frag1 == frag2 == frag3 == "" and sentence != constructedsentence:#loser
+        display.fill(WHITE)
+        rendered_text = render_textrect(constructedsentence, my_font, my_rect, RED, WHITE, 1)
+        rendered_text_user = render_textrect(username, my_font, my_rect_user, BROWN, WHITE, 0)#last 0 is to left align
+        rendered_text_frag_1 = render_textrect(frag0, my_font, my_rect_frag_1, RED, WHITE, 0)
+        rendered_text_frag_2 = render_textrect(frag1, my_font, my_rect_frag_2, YELLOW, WHITE, 0)
+        rendered_text_frag_3 = render_textrect(frag2, my_font, my_rect_frag_3, GREEN, WHITE, 0)
+        rendered_text_frag_4 = render_textrect(frag3, my_font, my_rect_frag_4, BLUE, WHITE, 0)
+
+        display.blit(rendered_text, my_rect.topleft)
+        display.blit(rendered_text_user, my_rect_user.topleft)
+        display.blit(rendered_text_frag_1, my_rect_frag_1.topleft)
+        display.blit(rendered_text_frag_2, my_rect_frag_2.topleft)
+        display.blit(rendered_text_frag_3, my_rect_frag_3.topleft)
+        display.blit(rendered_text_frag_4, my_rect_frag_4.topleft)
+
+        pygame.display.update()
+        soundloss.play()
+
+        return
+
+    display.fill(WHITE)
+    rendered_text = render_textrect(constructedsentence, my_font, my_rect, BLACK, WHITE, 1)
+    rendered_text_user = render_textrect(username, my_font, my_rect_user, BROWN, WHITE, 0)#last 0 is to left align
+    rendered_text_frag_1 = render_textrect(frag0, my_font, my_rect_frag_1, RED, WHITE, 0)
+    rendered_text_frag_2 = render_textrect(frag1, my_font, my_rect_frag_2, YELLOW, WHITE, 0)
+    rendered_text_frag_3 = render_textrect(frag2, my_font, my_rect_frag_3, GREEN, WHITE, 0)
+    rendered_text_frag_4 = render_textrect(frag3, my_font, my_rect_frag_4, BLUE, WHITE, 0)
+
+    display.blit(rendered_text, my_rect.topleft)
+    display.blit(rendered_text_user, my_rect_user.topleft)
+    display.blit(rendered_text_frag_1, my_rect_frag_1.topleft)
+    display.blit(rendered_text_frag_2, my_rect_frag_2.topleft)
+    display.blit(rendered_text_frag_3, my_rect_frag_3.topleft)
+    display.blit(rendered_text_frag_4, my_rect_frag_4.topleft)
+
+    pygame.display.update()
+
+    return
+
+#connect to database
+try:
+    conn = sqlite3.connect('reader.db')
+except:
+    print("Database not found!")
+
+c = conn.cursor()
+
+pygame.init()
+
+pygame.mixer.init()
+
+soundwin = pygame.mixer.Sound(soundwinfile)
+soundloss = pygame.mixer.Sound(soundlossfile)
+
+display = pygame.display.set_mode((1024, 768))
+
+my_font = pygame.font.Font(None, 64)
+my_rect = pygame.Rect((20, 200, 984, 388))
+my_rect_user = pygame.Rect((20, 20, 984, 80))
+my_rect_frag_1 = pygame.Rect((20, 488, 984, 65))
+my_rect_frag_2 = pygame.Rect((20, 553, 984, 65))
+my_rect_frag_3 = pygame.Rect((20, 618, 984, 65))
+my_rect_frag_4 = pygame.Rect((20, 683, 984, 65))
+
+display.fill(WHITE)
+
+pygame.display.update()
+
+while not finished:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            finished = True
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_SPACE:
+                NewUser()
+            if event.key in (pygame.K_a,pygame.K_e,pygame.K_i,pygame.K_m,pygame.K_q,pygame.K_u):
+                if frag0 != "":
+                    RefreshScreen(frag0)
+                else:
+                    pass
+            if event.key in (pygame.K_b,pygame.K_f,pygame.K_j,pygame.K_n,pygame.K_r,pygame.K_v):
+                if frag1 != "":
+                    RefreshScreen(frag1)
+                else:
+                    pass
+            if event.key in (pygame.K_c,pygame.K_g,pygame.K_k,pygame.K_o,pygame.K_s,pygame.K_w):
+                if frag2 != "":
+                    RefreshScreen(frag2)
+                else:
+                    pass
+            if event.key in (pygame.K_d,pygame.K_h,pygame.K_l,pygame.K_p,pygame.K_t,pygame.K_x):
+                if frag3 != "":
+                    RefreshScreen(frag3)
+                else:
+                    pass
+            
+    pygame.display.update()
+
+conn.close()
