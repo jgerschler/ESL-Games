1d9905127d5976a246a8d5716818c267a2803536
Jared Gerschler <jjgerschler@gmail.com>
2017-09-25 15:45:44 -0500

    updated coloring

--- Timed Words Team Game/TimedWordsTeamGame.py
+++ Timed Words Team Game/TimedWordsTeamGame.py
@@ -7,6 +7,8 @@ class TimedWordsTeamGame(object):
     BLACK = (0, 0, 0)
     WHITE = (255, 255, 255)
     RED = (255, 0, 0)
+    YELLOW = (230, 230, 0)
+    GREEN = (0, 128, 0))
     BLUE = (0, 0, 255)
     INV_PLAY_TIME = 0.5
     NUM_TEAM_MEMBERS = 30
