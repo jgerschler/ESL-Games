b5c103e05088d3c503d824b4fb3caf7819c41928
unknown <ldulceolivia@gmail.com>
2017-10-17 13:33:50 -0500

    things in common words game is being created

---
+++ Things In Common Game/ThingsInCommonGame.py
@@ -0,0 +1,40 @@
+don't
+can't
+am not
+
+do
+can
+am
+
+{
+"""I don't like fish.""":[],
+"""I don't know how to swim.""":[],
+"""I don't like potatoes.""":[],
+"""I don't eat donuts at night.""":[],
+"""I don't watch scary movies.""":[],
+"""I can't ride a bicycle.""":[],
+"""I can't swim.""":[],
+"""I can't drive a car.""":[],
+"""I can't do a pushup.""":[],
+"""I can't run faster than a bus.""":[],
+"""I'm not fat.""":[],
+"""I'm not skinny.""":[],
+"""I'm not an angry person.""":[],
+"""I don't like fish""":[],
+"""I don't like fish""":[],
+"""I don't like fish""":[],
+"""I don't like fish""":[],
+"""I don't like fish""":[],
+"""I don't like fish""":[],
+"""I don't like fish""":[],
+"""I don't like fish""":[],
+"""I don't like fish""":[],
+"""I don't like fish""":[],
+"""I don't like fish""":[],
+"""I don't like fish""":[],
+"""I don't like fish""":[],
+"""I don't like fish""":[],
+"""I don't like fish""":[],
+"""I don't like fish""":[],
+"""I don't like fish""":[],
+}
