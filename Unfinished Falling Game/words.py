cf0fd9a5e5f420392e6f5ad7abb0bd05679eea0c
Jared Gerschler <jjgerschler@gmail.com>
2018-04-09 12:16:36 -0500

    building new past participle game

--- Unfinished Falling Game/words.py
+++ Unfinished Falling Game/words.py
@@ -6,15 +6,47 @@ WHITE = (255, 255, 255)
 LIGHTGREY = (192, 192, 192)
 DARKGREY = (128, 128, 128)
 
+adjectives = ["red","orange","yellow","green","blue","purple", "fat",
+              "skinny", "happy", "sad"]
+verbs = ["be", "beat", "begin", "bite", "blow", "break",
+         "bring", "build", "buy", "catch", "choose", "cost",
+         "do", "draw", "drink", "drive", "eat", "fall", "feed",
+         "feel", "fight", "find", "fly", "forget", "freeze", "get",
+         "give", "go", "grow", "hang", "have", "hear", "hide",
+         "hold", "keep", "know", "lead", "leave", "lend",
+         "lose", "make", "mean", "meet", "pay", "ride",
+         "ring", "rise", "say", "see", "sell", "send", "set",
+         "shoot", "shut", "sing", "sit", "sleep", "speak", "spend",
+         "stand", "steal", "stick", "swim", "take", "teach", "tell",
+         "think", "throw", "wake", "wear", "win", "write"]
+past_participles = ["been", "beaten", "begun", "bitten", "blown",
+                    "broken", "brought", "built",
+                    "bought", "caught", "chosen", "cost",
+                    "done", "drawn", "drunk", "driven",
+                    "eaten", "fallen", "fed", "felt", "fought",
+                    "found", "flown", "forgotten", "frozen",
+                    "gotten", "given", "gone", "grown", "hung",
+                    "had", "heard", "hidden", "held",
+                    "kept", "known", "led", "left",
+                    "lent", "lost", "made", "meant",
+                    "met", "paid", "ridden",
+                    "rung", "risen", "said", "seen",
+                    "sold", "sent", "set", "shot", "shut",
+                    "sung", "sat", "slept", "spoken", "spent",
+                    "stood", "stolen", "stuck", "swum", "taken",
+                    "taught", "told", "thought", "thrown",
+                    "woken", "worn", "won", "written"]
+pronouns = ["I", "We", "You", "He", "She", "It", "They",
+            "Me", "You", "Us", "Him", "Her", "It", "Them"]
+
+global words
+words = adjectives + verbs + past_participles + pronouns
+
 class Word(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.words = ['eat','talk','watch','walk',
-                'work','sleep','sing','red',
-                'orange','yellow','green','blue',
-                'purple']
         self.font = pygame.font.Font(None, random.randint(32, 96))
-        self.word = random.choice(self.words)
+        self.word = random.choice(words)
         self.image = self.font.render(self.word, 1, (random.randint(100, 255), random.randint(100, 255), random.randint(100, 255)))
         self.rect = self.image.get_rect()
         self.speed = random.randint(1, 8)
@@ -24,7 +56,10 @@ class Word(pygame.sprite.Sprite):
         self.rect.x = random.randrange(0, screen_width)
 
     def update(self):
-        self.rect.y += self.speed
+        if score > 0:
+            self.rect.y += (self.speed * score / 5) + 1
+        else:
+            self.rect.y += 1
         if self.rect.y > screen_height:
             self.reset_pos()
 
@@ -33,12 +68,16 @@ class Crosshair(Word):
     def __init__(self):
         super().__init__()
         self.image = pygame.image.load('images\\crosshair.png')
-        self.rect = self.image.get_rect()        
+        self.rect = self.image.get_rect()
         
     def update(self):
         pos = pygame.mouse.get_pos()
         self.rect.x = pos[0]
         self.rect.y = pos[1]
+
+def game_over():
+    
+    pygame.time.delay(5000)
 
 pygame.init()
 pygame.mixer.init()
@@ -50,6 +89,7 @@ pygame.mouse.set_visible(0)
 
 laser_sound = pygame.mixer.Sound('audio\\laser.ogg')
 explosion_sound = pygame.mixer.Sound('audio\\explosion.ogg')
+scream_sound = pygame.mixer.Sound('audio\\scream.ogg')
 explosion_image = pygame.image.load('images\\explosion.png')
 
 font = pygame.font.Font(None, 128)
@@ -70,7 +110,13 @@ for medium_stars in range(60):
 word_list = pygame.sprite.Group()
 all_sprites_list = pygame.sprite.Group()
 
-for i in range(20):
+global score
+score = 0
+
+previous_time = 0
+time_remaining = 60
+
+for i in range(50):
     word = Word()
 
     word.rect.x = random.randrange(screen_width)
@@ -80,23 +126,21 @@ for i in range(20):
     all_sprites_list.add(word)
 
 crosshair = Crosshair()
+
 all_sprites_list.add(crosshair)
 
 done = False
 
 clock = pygame.time.Clock()
-score = 0
-previous_time = 0
-time_remaining = 60
 
-adjectives = ['red','orange','yellow','green','blue','purple']
-verbs = ['eat','talk','watch','walk','work','sleep','sing']
+
+
 
 while not done:
     if pygame.time.get_ticks() - previous_time >= 1000:
         previous_time = pygame.time.get_ticks()
         time_remaining -= 1
-        if time_remaining < 0:
+        if time_remaining == 0:
             done = True
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -106,18 +150,17 @@ while not done:
         if event.type == pygame.MOUSEBUTTONDOWN:
             word_hit_list = pygame.sprite.spritecollide(crosshair, word_list, False)
             if len(word_hit_list) > 0:
-                explosion_sound.play()
                 screen.blit(explosion_image, (crosshair.rect.x - 60, crosshair.rect.y - 60))
                 pygame.display.update()
                 for word in word_hit_list:
                     all_sprites_list.remove(word)
                     word_list.remove(word)
-                    if word.word in verbs:
-                        print("you killed a verb!")
+                    if word.word in past_participles:
+                        explosion_sound.play()
                         score += 1
                     else:
-                        print("argh! you killed an adjective")
-                        score += 2
+                        scream_sound.play()
+                        score -= 1
             else:
                 laser_sound.play()
 
@@ -142,7 +185,6 @@ while not done:
             bg_star[0] = random.randrange(0, screen_width)
             bg_star[1] = random.randrange(-20, -5)
         pygame.draw.circle(screen, LIGHTGREY, bg_star, 3)
-
     
     all_sprites_list.update()
     all_sprites_list.draw(screen)
